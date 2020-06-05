2ac94afd277ec2311034b00d42ee5d6994f38d5a
Lewis <ljleighfield@gmail.com>
2020-06-05 20:42:54 +0100

    give 'index.html' access to categories

--- index/views.py
+++ index/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from products.models import *
 
 
 def index(request):
-    return render(request, "index/index.html")
+    all_categories = Category.objects.all()
+    all_subcategories = SubCategory.objects.all()
+    context = {
+        "all_categories": all_categories,
+        "all_subcategories": all_subcategories,
+    }
+    return render(request, "index/index.html", context)
