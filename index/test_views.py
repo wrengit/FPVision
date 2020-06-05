52d06e3cc6bd19b7a8e42f2c7a7b3210826d283b
Lewis <ljleighfield@gmail.com>
2020-06-05 13:10:04 +0100

    add base templates + create index app

---
+++ index/test_views.py
@@ -0,0 +1,8 @@
+from django.test import TestCase
+
+
+class TestViews(TestCase):
+    def test_base_index_page(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "index/index.html")
