174916c9354c08c293f88c2d4935eda9f48d1f5d
Lewis <ljleighfield@gmail.com>
2020-06-07 20:20:22 +0100

    add JS live search functionality

--- search/urls.py
+++ search/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 from . import views
 
 
-urlpatterns = [path("", views.search_result, name="search_result")]
+urlpatterns = [
+    path("", views.search_result, name="search_result"),
+    path("js_search/", views.js_search, name="js_search"),
+]
--- search/views.py
+++ search/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.http import JsonResponse
+from django.contrib import messages
 from products.models import Category, SubCategory, Product
 from django.db.models import Q
+from django.core import serializers
 
 
 def search_result(request):
@@ -9,7 +12,10 @@ def search_result(request):
     products = None
     query = None
     if "q" in request.GET:
-        query = request.GET.get("q")
+        query = request.GET["q"]
+        if not query:
+            messages.error(request, "Please enter a search criteria")
+            return redirect(reverse("all_products"))
         products = Product.objects.all().filter(
             Q(name__contains=query)
             | Q(description__contains=query)
@@ -23,3 +29,29 @@ def search_result(request):
         "all_subcategories": all_subcategories,
     }
     return render(request, "search/search.html", context)
+
+
+def js_search(request):
+    products = None
+    filter_products = None
+    query = None
+    if "q" in request.GET:
+        query = request.GET["q"]
+        products = Product.objects.all().filter(
+            Q(name__contains=query)
+            | Q(description__contains=query)
+            | Q(category__name__contains=query)
+            | Q(sub_category__name__contains=query)
+        )
+        filter_products = products.values(
+            "name",
+            "price",
+            "category",
+            "sub_category",
+            "image",
+            "image_url",
+            "id",
+            "slug",
+        ).filter(available=True)
+
+    return JsonResponse(list(filter_products), safe=False)
