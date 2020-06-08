8a51e90ea9f8d75b559400529a15021e9c77cc8b
Lewis <ljleighfield@gmail.com>
2020-06-08 14:13:56 +0100

    add tests for search.views

--- search/test_views.py
+++ search/test_views.py
@@ -1,10 +1,10 @@
 from django.test import TestCase, RequestFactory
-from products.models import Category, SubCategory, Product
+from django.shortcuts import reverse
+from django.contrib import messages
+from django.contrib.messages import get_messages
+from products.models import *
 from django.db.models import Q
 
-"""
-last two tests require refactoring to satisfy Coverage's demands
-"""
 
 class TestViews(TestCase):
     def test_search_view(self):
@@ -13,7 +13,9 @@ class TestViews(TestCase):
         self.assertTemplateUsed(response, "search/search.html")
 
     def test_search_by_product_name(self):
-        test_category = Category.objects.create(name="test_category", slug="test-category")
+        test_category = Category.objects.create(
+            name="test_category", slug="test-category"
+        )
         test_subcategory = SubCategory.objects.create(
             name="test_subcategory", slug="test-subcategory", category=test_category
         )
@@ -24,29 +26,82 @@ class TestViews(TestCase):
             sub_category=test_subcategory,
             stock=1,
             price=1,
+        )
+        url = "{url}?{filter}={value}".format(
+            url=reverse("search_result"), filter="q", value="test"
         )
         query = "test"
-        products = Product.objects.all().filter(Q(name__contains=query))
-        self.assertIn(test_product, products)
-        
+        response = self.client.get(url)
+        test_products = Product.objects.all().filter(Q(name__contains=query))
+        self.assertQuerysetEqual(
+            response.context["products"], test_products, transform=lambda x: x
+        )
+
     def test_search_by_product_description(self):
-        test_category = Category.objects.create(name="test_category", slug="test-category")
+        test_category = Category.objects.create(
+            name="test_category", slug="test-category"
+        )
         test_subcategory = SubCategory.objects.create(
             name="test_subcategory", slug="test-subcategory", category=test_category
         )
         test_product = Product.objects.create(
             name="test_product",
             slug="test-product",
-            description = "a test string",
+            description="a test string",
             category=test_category,
             sub_category=test_subcategory,
             stock=1,
             price=1,
         )
+        url = "{url}?{filter}={value}".format(
+            url=reverse("search_result"), filter="q", value="string"
+        )
         query = "string"
-        products = Product.objects.all().filter(Q(description__contains=query))
-        self.assertIn(test_product, products)
-      
-    
-        
-       
+        response = self.client.get(url)
+        test_products = Product.objects.all().filter(Q(description__contains=query))
+        self.assertQuerysetEqual(
+            response.context["products"], test_products, transform=lambda x: x
+        )
+
+    def test_correct_error_message(self):
+        url = "{url}?{filter}={value}".format(
+            url=reverse("search_result"), filter="q", value=""
+        )
+        response = self.client.get(url)
+        messages = [m.message for m in get_messages(response.wsgi_request)]
+        self.assertIn("Please enter a search criteria", messages)
+
+    def test_search_by_product_name(self):
+        test_category = Category.objects.create(
+            name="test_category", slug="test-category"
+        )
+        test_subcategory = SubCategory.objects.create(
+            name="test_subcategory", slug="test-subcategory", category=test_category
+        )
+        test_product = Product.objects.create(
+            name="test_product",
+            price=1,
+            category=test_category,
+            sub_category=test_subcategory,
+            stock=1,
+        )
+        url = "{url}?{filter}={value}".format(
+            url=reverse("js_search"), filter="q", value="test"
+        )
+        query = "test"
+        response = self.client.get(url)
+        self.assertJSONEqual(
+            response.content.decode("utf8"),
+            [
+                {
+                    "name": "test_product",
+                    "price": "1.00",
+                    "category": "test_category",
+                    "sub_category": "test_subcategory",
+                    "image": "/static/media/defaults/default_product.jpg",
+                    "image_url": None,
+                    "id": 1,
+                    "slug": "",
+                }
+            ],
+        )
