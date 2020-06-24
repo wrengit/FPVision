7275e1697f329d1ca97fe40fe3924ec3f2b6d7c7
Lewis <ljleighfield@gmail.com>
2020-06-24 09:02:39 +0100

    add profile models

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django_countries.fields import CountryField
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    default_phone_number = models.CharField(
+        max_length=20, blank=False, null=True
+    )
+    default_country = CountryField(
+        blank_label="Country *", blank=False, null=True
+    )
+    default_postcode = models.CharField(max_length=20, blank=False, null=True)
+    default_post_town = models.CharField(max_length=40, blank=False, null=True)
+    default_address_1 = models.CharField(max_length=80, blank=False, null=True)
+    default_address_2 = models.CharField(max_length=80, blank=False, null=True)
+    default_county = models.CharField(max_length=80, blank=False, null=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+    instance.userprofile.save()
