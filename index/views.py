f4dc33373399f8f2ece3b853ab658dcd392b326f
Lewis <ljleighfield@gmail.com>
2020-07-03 14:35:56 +0100

    make free delivery var available to index view

--- index/views.py
+++ index/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from django.conf import settings
 
 
 def index(request):
 
-    return render(request, "index/index.html")
+    return render(
+        request,
+        "index/index.html",
+        context={"free_delivery": settings.FREE_DELIVERY_THRESHOLD},
+    )
