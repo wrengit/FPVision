dfdf4d325a22b1b404c4f69abce361ed9fc980f7
Lewis <ljleighfield@gmail.com>
2020-07-06 12:06:12 +0100

    attempt to fix DB_URL parsing

--- FPVision/settings.py
+++ FPVision/settings.py
@@ -126,7 +126,7 @@ WSGI_APPLICATION = "FPVision.wsgi.application"
 
 if "DATABASE_URL" in os.environ:
     DATABASES = {
-        "default": dj_database_url.parse(urllib.parse.quote(os.environ.get("DATABASE_URL"), ":/@"))
+        "default": dj_database_url.parse(urllib.parse.quote(os.environ.get("DATABASE_URL"), "-:/@"))
     }
 else:
     DATABASES = {
