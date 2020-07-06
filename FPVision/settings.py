4d593b8ce3c9cba7bdc7a09b497bfa7533d1e720
Lewis <ljleighfield@gmail.com>
2020-07-06 15:57:00 +0100

    correct var spelling

--- FPVision/settings.py
+++ FPVision/settings.py
@@ -178,7 +178,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-STATICROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
