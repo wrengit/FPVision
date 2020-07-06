679796937d2019e5f1cb5d5c971f9afd9761186f
Lewis <ljleighfield@gmail.com>
2020-07-06 11:47:24 +0100

    chnage AWS region name

--- FPVision/settings.py
+++ FPVision/settings.py
@@ -27,6 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -184,7 +185,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 if "USE_AWS" in os.environ:
     # Bucket config
     AWS_STORAGE_BUCKET_NAME = "fpvision"
-    AWS_S3_REGION_NAME = "eu-london"
+    AWS_S3_REGION_NAME = "eu-west-2"
     AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
     AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
     AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
