4a839b922dbb37a00f2b1b39742bb13270f58b13
Lewis <ljleighfield@gmail.com>
2020-07-06 15:58:29 +0100

    collect static

---
+++ staticfiles/profiles/js/country_select.js
@@ -0,0 +1,11 @@
+let countrySelected = document.getElementById("id_default_country")
+if (!countrySelected.value) {
+    countrySelected.style.color = "rgba(54, 54, 54, 0.3)"
+}
+countrySelected.onchange = () => {
+    if (!countrySelected.value) {
+        countrySelected.style.color = "rgba(54, 54, 54, 0.3)"
+    } else {
+        countrySelected.style.color = "#363636"
+    }
+}
