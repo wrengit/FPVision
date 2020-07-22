d7323f5155cb92e11f67ebc3286278d21a1770c2
Lewis Leighfield <ljleighfield@gmail.com>
2020-07-22 12:48:57 +0100

    add email backends for gmail

--- FPVision/settings.py
+++ FPVision/settings.py
@@ -110,7 +110,6 @@ AUTHENTICATION_BACKENDS = (
 
 SITE_ID = 1
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 ACCOUNT_AUTHENTICATION_METHOD = "username_email"
 ACCOUNT_EMAIL_REQUIRED = True
@@ -147,18 +146,9 @@ AUTH_PASSWORD_VALIDATORS = [
         "NAME": "django.contrib.auth.password_validation."
         + "UserAttributeSimilarityValidator",
     },
-    {
-        "NAME": "django.contrib.auth.password_validation." +
-        "MinimumLengthValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation." +
-        "CommonPasswordValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation." +
-        "NumericPasswordValidator",
-    },
+    {"NAME": "django.contrib.auth.password_validation." + "MinimumLengthValidator",},
+    {"NAME": "django.contrib.auth.password_validation." + "CommonPasswordValidator",},
+    {"NAME": "django.contrib.auth.password_validation." + "NumericPasswordValidator",},
 ]
 
 
@@ -222,3 +212,15 @@ STRIPE_WH_SECRET = env("STRIPE_WH_SECRET")
 COUNTRIES_FIRST = ["GB"]
 
 COUNTRIES_FIRST_REPEAT = True
+
+if "DEVELOPMENT" in os.environ:
+    EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+    DEFAULT_FROM_EMAIL = "fpvision@example.com"
+else:
+    EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = "smtp.gmail.com"
+    EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+    EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASS")
+    DEFAULT_FROM_EMAIL = os.environ.get("EMAIL_HOST_USER")
