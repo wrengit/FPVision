9727b4635ff75e54fccdf9e2fbfbcad3368b36e5
Lewis Leighfield <ljleighfield@gmail.com>
2020-07-23 11:17:45 +0100

    make checkout country select grayed out

---
+++ checkout/static/checkout/js/country_selected_checkout.js
@@ -0,0 +1,14 @@
+window.onload = () => {
+    let countrySelected = document.getElementById("id_country");
+    if (!countrySelected.value) {
+      countrySelected.style.color = "rgba(54, 54, 54, 0.3)";
+    }
+    countrySelected.onchange = () => {
+      if (!countrySelected.value) {
+        countrySelected.style.color = "rgba(54, 54, 54, 0.3)";
+      } else {
+        countrySelected.style.color = "#363636";
+      }
+    };
+  };
+  
--- profiles/static/profiles/js/country_select.js
+++ profiles/static/profiles/js/country_select.js
@@ -1,11 +1,13 @@
-let countrySelected = document.getElementById("id_default_country")
-if (!countrySelected.value) {
-    countrySelected.style.color = "rgba(54, 54, 54, 0.3)"
-}
-countrySelected.onchange = () => {
+window.onload = () => {
+  let countrySelected = document.getElementById("id_default_country");
+  if (!countrySelected.value) {
+    countrySelected.style.color = "rgba(54, 54, 54, 0.3)";
+  }
+  countrySelected.onchange = () => {
     if (!countrySelected.value) {
-        countrySelected.style.color = "rgba(54, 54, 54, 0.3)"
+      countrySelected.style.color = "rgba(54, 54, 54, 0.3)";
     } else {
-        countrySelected.style.color = "#363636"
+      countrySelected.style.color = "#363636";
     }
-}
+  };
+};
